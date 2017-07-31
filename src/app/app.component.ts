faa2257ce26dedc837f882fb555e3fd1237394ed
Stephen Jenkinson <stephen.jenkinson@Stephens-MacBook-Pro.local>
2017-07-31 15:31:52 +1000

    add count down page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  public days: number = 0;
+  public hours: number = 0;
+  public minutes: number = 0;
+  public seconds: number = 0;
+
+  constructor() {
+
+    const countDownDate = new Date("Aug 7, 2017 08:30:00").getTime();
+
+    // Update the count down every 1 second
+    setTimeout(() => {
+
+      // Get todays and difference between dates
+      const now = new Date().getTime();
+      const distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    }, 1000);
+
+  }
+
 }
